d93a08bd557acaaa0674253736593915a22cd010
Jake Mcneill <jake-cryptic@users.noreply.github.com>
2020-03-04 21:56:18 +0000

    Added new templates and updated CSS

--- store/routes.py
+++ store/routes.py
@@ -1,9 +1,26 @@
-from flask import render_template, url_for, request, redirect, flash
+from flask import render_template, url_for, request, redirect, flash, make_response
 from store import app
 from store.models import User, Billing, Address, OrderProducts, Orders, Products
 
+# App routes
 @app.route("/")
-@app.route("/home")
+@app.route("/home", methods=['GET'])
 def home():
-  return render_template('home.html')
+	return render_template('home.html')
 
+@app.route("/account", methods=['GET'])
+def account():
+	return render_template('account.html')
+
+@app.route("/basket", methods=['GET'])
+def basket():
+	return render_template('basket.html')
+
+@app.route("/product/<int:product_id>", methods=['GET'])
+def product(product_id):
+	return render_template('product.html', product_id=product_id)
+
+# Error routes
+@app.errorhandler(404)
+def not_found(msg):
+	return make_response(render_template("error_404.html", msg=msg), 404)
