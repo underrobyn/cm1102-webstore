34c5489f764194f65ac1039801dc09a404ca144d
Jake Mcneill <mcneillj@cardiff.ac.uk>
2020-03-11 15:59:43 +0000

    Updated requirements

--- store/config.py
+++ store/config.py
@@ -1,3 +1,3 @@
-db_uri = 'mysql+pymysql://USERNAME:PASSWORD@csmysql.cs.cf.ac.uk:3306/USERNAME'
+db_uri = 'mysql+pymysql://u953270795_webstore:FSDF82Jlkaoqtnm122s@server150.main-hosting.eu:3306/u953270795_webstore'
 
 secret_key = ''
