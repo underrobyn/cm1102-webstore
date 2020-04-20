d8535a78168ed275d466456dbc23f22493da5727
Jake Mcneill <mcneillj@cardiff.ac.uk>
2020-04-20 22:25:17 +0100

    Added forms for login and create account Added content to account page

--- store/config.py
+++ store/config.py
@@ -1,3 +1,3 @@
 db_uri = 'mysql+pymysql://u953270795_webstore:FSDF82Jlkaoqtnm122s@server150.main-hosting.eu:3306/u953270795_webstore'
 
-secret_key = ''
+secret_key = 'uerhg34g35gm'
--- store/routes.py
+++ store/routes.py
@@ -9,19 +9,19 @@ from flask_admin.contrib.sqla import ModelView
 @app.route("/")
 @app.route("/home", methods=['GET'])
 def home():
-	return render_template('home.html')
+    return render_template('home.html')
 
 @app.route("/account", methods=['GET'])
 def account():
-	return render_template('account.html')
+    return render_template('account.html')
 
 @app.route("/basket", methods=['GET'])
 def basket():
-	return render_template('basket.html', title='Basket')
+    return render_template('basket.html', title='Basket')
 
 @app.route("/product/<int:product_id>", methods=['GET'])
 def product(product_id):
-	return render_template('product.html', product_id=product_id)
+    return render_template('product.html', product_id=product_id)
 
 # Admin views
 admin.add_view(ModelView(User, db.session))
@@ -32,21 +32,51 @@ admin.add_view(ModelView(Orders, db.session))
 admin.add_view(ModelView(Products, db.session))
 
 # Account system routes
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+
+        if user is None:
+            flash('Invalid email address')
+        elif not user.verify_password(form.password.data):
+            flash('Invalid password')
+        else:
+            login_user(user)
+            flash('You have been logged in')
+            return redirect(url_for('home'))
+
+        return render_template('login.html', form=form)
+
+    return render_template('login.html', title='Login', form=form)
+
 @app.route("/create",methods=['GET','POST'])
 def create():
-	return render_template('create.html', title='Login')
+    form = CreateForm()
+    if form.validate_on_submit():
+        new_user = User(
+            name=form.name.data,
+            email=form.email.data,
+            password=form.password.data
+        )
 
-@app.route("/login",methods=['GET','POST'])
-def login():
-	return render_template('login.html', title='Login')
+        db.session.add(new_user)
+        db.session.commit()
+
+        flash('Account created')
+
+        return redirect(url_for('home'))
+
+    return render_template('create.html', title='Create Account', form=form)
 
 @app.route("/logout")
 def logout():
-	logout_user()
-	flash('Logout successful!')
-	return redirect(url_for('home'))
+    logout_user()
+    flash('Logout successful!')
+    return redirect(url_for('home'))
 
 # Error routes
 @app.errorhandler(404)
 def not_found(msg):
-	return make_response(render_template("error_404.html", msg=msg), 404)
+    return make_response(render_template("error_404.html", msg=msg), 404)
